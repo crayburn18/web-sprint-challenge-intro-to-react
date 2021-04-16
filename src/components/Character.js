41f0d13174a7d7fafa796d48b05aaaf78da08b10
Connor Rayburn <connorrayburn.work@gmail.com>
2021-04-16 12:43:49 -0700

    correct number of styled divs redered but not the data inside

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,26 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledCharacter = styled.div`
+    color: white;
+    background: black;
+    opacity: 0.65;
+    padding: 10px;
+    width: 40%;
+    margin: 2px auto;
+    border-radius: 7px;
+    &:hover{
+        background: red;
+    }
+    font-size: 1.5em;
+`
+
+export default function Character({characters}){
+
+    return(  
+        <StyledCharacter>
+           {characters.name}
+        </StyledCharacter> 
+    );
+};
